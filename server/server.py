0164bb807f0042a3aba8a68dbbb9386e9b3568d2
Sebastian-De-Jesus <sreyes92@csu.fullerton.edu>
2023-04-24 11:57:10 -0700

    Fixed issue with 'ls' command

--- server/server.py
+++ server/server.py
@@ -3,12 +3,13 @@ import threading
 import sys
 import os
 
-HEADER = 4096
+HEADER = 2048
 PORT = int(sys.argv[1])
 SERVER = socket.gethostbyname(socket.gethostbyname("localhost"))
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DC = "GOODBYE"
+COMMAND = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -19,19 +20,22 @@ def start():
     print(f"Server is [LISTENING] on {SERVER}\n")
     while True:
         conn, addr = server.accept()
+        print(conn)
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[Connections currently Active] {threading.active_count() - 1}\n")
 
-#This code allows us to print the current working directory
-def list_files_in_current_directory():
+
+# This code allows us to print the current working directory by passing the CREATED CONNN
+def list_files_in_current_directory(conn):
     current_directory = os.getcwd()
     files_and_directories = os.listdir(current_directory)
-    
+    #Returns to the client all the files in the directory
     for item in files_and_directories:
-        print(item)
+        conn.send(f"{item}\n".encode(FORMAT))
 
-#This code enables us to use the "Get" command in our current working dirrectory: 
+
+# This code enables us to use the "Get" command in our current working dirrectory:
 
 def get_file_content(file_name):
     current_directory = os.getcwd()
@@ -45,31 +49,34 @@ def get_file_content(file_name):
         print(f"'{file_name}' does not exist in the current directory.")
         return None
 
+
 def handle_client(conn, addr):
     print(f"[New connection Incoming] {addr} has connected.\n")
 
-    #keeping the connection alive with a while loop untill client enters quit
+    # keeping the connection alive with a while loop untill client enters quit
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        #checks for msg after the initial blank message that initiates the connection
+        # checks for msg after the initial blank message that initiates the connection
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DC:
                 connected = False
-            print(f"[{SERVER} has sent {msg}")
-            
-            #if users enters ls command we print out whats in our CWD
+            print(f"[{ADDR}] has sent '{msg}'!")
+
+            # if users enters ls command we print out whats in our CWD
             if msg == "ls":
-            	list_files_in_current_directory()
-            	
-            
-            
+                list_files_in_current_directory(conn)
+            elif msg != "ls":
+                COMMAND = msg.split()
+                if COMMAND[0] == "get":
+                    get_file_content(COMMAND[1])
+
     conn.close()
 
 
-print("[STARTING] server i215s starting.....")
+print("[STARTING] server starting.....")
 
 start()
 
