e8fb3dd579772cbe9730d2518420c9f0b9c2d0c2
Sebastian-De-Jesus <sreyes92@csu.fullerton.edu>
2023-04-25 21:37:28 -0700

    put command is now working properly from client to server

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ import os
 HEADER = 2048
 PORT = int(sys.argv[2])
 FORMAT = "utf-8"
-NOFILE = "FILE DOES NOT EXISIT"
+NOFILE = "FILE DOES NOT EXIST"
 DC = "GOODBYE"
 SERVER = "localhost"
 
@@ -15,7 +15,8 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-#Here our funciton simple formats the message to be sent to serverS
+
+# Here our funciton simple formats the message to be sent to serverS
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
@@ -25,32 +26,54 @@ def send(msg):
     client.send(message)
     print(client.recv(HEADER).decode(FORMAT))
 
-#In this function we follow the same syntax as above with slight modification because we are saving the
-#recieved msg from the server, we additionaly are passing a filename to the function
 
-def send_get_message(msg,filename):
+# In this function we follow the same syntax as above with slight modification because we are saving the
+# recieved msg from the server, we additionaly are passing a filename to the function
+
+def send_get_message(msg, filename):
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    #Response recieved from server saved in a variable we will use to confirm if file exit and we can write 
-    #to our client floder
+    # Response recieved from server saved in a variable we will use to confirm if file exit and we can write
+    # to our client floder
     recieved_response = client.recv(HEADER).decode(FORMAT)
     if recieved_response == NOFILE:
-    	print(NOFILE)
+        print(NOFILE)
     else:
         current_directory = os.getcwd()
-        file_path = os.path.join(current_directory,filename)
+        file_path = os.path.join(current_directory, filename)
         with open(file_path, 'w') as file:
             file.write(recieved_response)
-    
+
+def send_put_message(msg, file_name):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, file_name)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return client.send(f"{content}\n".encode(FORMAT))
+    else:
+        print("[Error] FILE DOES NOT EXIST")
+        client.send(NOFILE.encode(FORMAT))
+        return None
+
+
 connected = True
 while connected:
 
     command = input("ftp> ".split())
-    
+
     if len(command) == 0:
         continue
     # allows user to exit with the following quit command
@@ -61,15 +84,17 @@ while connected:
     # allows users to list the conntents of CWD
     elif command == "ls":
         send(command)
-        
+
 
     # Here we are sending over the command of get/pull which will be dealt with on the server side
     else:
         command_chosen = command.split()
         if command_chosen[0] == "get":
-            send_get_message(command,command_chosen[1])
-
-
-
-
+            if len(command_chosen) > 1: 
+                send_get_message(command, command_chosen[1])
+            else:
+                print("[ERROR] no specified txt file to Get")
+        if command_chosen[0] == "put":
+            if len(command_chosen) > 1:
+                send_put_message(command, command_chosen[1])
 
